ec0991ff7311715c98d9e590c459e4f9868262b7
Njyhier <njerinjihia43@gmail.com>
2026-03-18 12:51:11 +0300

    Added products page component and [its template

---
+++ src/app/pages/products/productspage-component/productspage-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productspage-component',
+  imports: [],
+  templateUrl: './productspage-component.html',
+  styleUrl: './productspage-component.css',
+})
+export class ProductspageComponent {
+
+}
