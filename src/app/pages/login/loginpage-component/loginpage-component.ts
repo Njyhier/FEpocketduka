11a314921f0363c08c03f0d9423da744715048f1
Njyhier <njerinjihia43@gmail.com>
2026-03-18 21:39:40 +0300

    Added login page

---
+++ src/app/pages/login/loginpage-component/loginpage-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loginpage-component',
+  imports: [],
+  templateUrl: './loginpage-component.html',
+  styleUrl: './loginpage-component.css',
+})
+export class LoginpageComponent {
+
+}
